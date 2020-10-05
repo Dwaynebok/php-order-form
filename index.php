1bd6798a0d640963b3a9539f327edb5943161572
Dwaynebok <dwaynebok19@gmail.com>
2020-10-05 10:20:12 +0200

    adding the display error code

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 //this line makes PHP behave in a more strict way
 declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 //we are going to use session variables so we need to enable sessions
 session_start();
