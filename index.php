6fda00844c941d637c7d8b92a1eb08bc9f9c01c6
Dwaynebok <dwaynebok19@gmail.com>
2020-10-07 16:41:15 +0200

    cleaning out a cookie code that didnt work

--- index.php
+++ index.php
@@ -175,20 +175,6 @@ if (isset($_POST['express_delivery'])){
 } else{
     $time = ' your food will arrive in 2hours';
 }
-/*
-//Making the counter with cookie
-$cookie_name = "count";
-$cookie_value = "$cookie_name";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-
-if (!isset($_COOKIE[$cookie_name])) {
-      echo "Cookie named '" . $cookie_name . "' is not set!";
-} else{
-    echo "Cookie '" . $cookie_name . "' is set!<br>";
-    echo "Value is: " . $_COOKIE[$cookie_name];
-
-}
-*/
 
 
 /*
